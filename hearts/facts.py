66270a2cb099d99086d2ed15f3172bcb8aa4e163
Zac Todd <zactodd0@gmail.com>
2021-10-23 11:39:59 +1300

    Update facts.py

--- hearts/facts.py
+++ hearts/facts.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from hearts.card import Card
 
 
 class SUIT(Enum):
@@ -6,3 +7,22 @@ class SUIT(Enum):
     SPADES = 'SPADES'
     HEARTS = 'HEARTS'
     DIAMONDS = 'DIAMONDS'
+
+
+class FACE(Enum):
+    ACE = 'A'
+    TWO = '2'
+    THREE = '3'
+    FOUR = '4'
+    FIVE = '5'
+    SIX = '6'
+    SEVEN = '7'
+    EIGHT = '8'
+    NINE = '9'
+    TEN = '10'
+    JACK = 'J'
+    QUEEN = 'Q'
+    KING = 'K'
+
+
+DECK = tuple(Card(f, s) for f in FACE for s in SUIT)
