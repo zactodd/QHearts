1a1f6f07bb7a12fe53e45b47adf742d9f436412d
Zac Todd <zactodd0@gmail.com>
2021-10-23 12:16:14 +1300

    hashable card

--- hearts/card.py
+++ hearts/card.py
@@ -4,7 +4,19 @@ class Card:
         self.suit = suit
 
     def __str__(self) -> str:
-        return f'{self.suit.value[0]}{self.face.value}'
+        return f'{self.suit.value}{self.face.value}'
 
     def __repr__(self):
         return self.__str__()
+
+    def __key(self):
+        return self.face, self.suit
+
+    def __hash__(self) -> int:
+        return hash(self.__key())
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Card):
+            return self.__key() == other.__key()
+        return NotImplemented
+
