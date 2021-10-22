839585c8a51bc383801e74bf7dea0aa7774ee14d
Zac Todd <zactodd0@gmail.com>
2021-10-23 12:44:19 +1300

    Create utils.py

---
+++ hearts/utils.py
@@ -0,0 +1,14 @@
+from hearts import facts
+from hearts.card import Card
+from typing import List, Set
+import random
+
+
+def shuffle_deck() -> List['Card']:
+    d = list(facts.DECK)
+    random.shuffle(d)
+    return d
+
+
+def str_to_card(s: str) -> Set['Card']:
+    return {Card(facts.SUIT(cs[0]), facts.FACE(cs[1:])) for cs in s.split()}
