8840fed1c0177dcb3df7dbcb3316c21a15af582a
Zac Todd <zactodd0@gmail.com>
2021-10-23 17:30:52 +1300

    Create env.py

---
+++ ql/env.py
@@ -0,0 +1,19 @@
+from gym import Env
+from gym.spaces import Discrete, Box
+import numpy as np
+import random
+
+
+class HeartsEnv(Env):
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+        # return self.state, reward, done, info
+
+    def render(self):
+        pass
+
+    def reset(self):
+        pass
